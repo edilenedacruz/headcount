bd288bca6d57c84588707ac8e74867fd74c6cd0c
Edilene <edilene_cruz@yahoo.com.br>
2017-01-10 20:08:55 -0700

    moving on

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -1,8 +1,33 @@
 require 'csv'
+require_relative '../lib/statewide_test_repository'
+
 class StatewideTest
-  attr_reader :name, :third_grade
+  attr_reader :name, :third_grade, :eighth_grade, :math, :reading, :writing
+
   def initialize(statewide_test_data)
     @name = statewide_test_data[:name].upcase
-    @third_grade = statewide_test_data[:third_grade]
+    @third_grade = statewide_test_data[:third_grade] || {}
+    @eighth_grade = statewide_test_data[:eighth_grade] || {}
+    @math = statewide_test_data[:math] || {}
+    @reading = statewide_test_data[:reading] || {}
+    @writing = statewide_test_data[:writing] || {}
   end
+
+  # def proficient_by_grade(grade)
+  #
+  # end
+  #
+  # def proficient_by_race_or_ethnicity(race)
+  #
+  # end
+  #
+  # def proficient_for_subject_by_grade_in_year(subject, grade, year)
+  #
+  # end
+  #
+  # def proficient_for_subject_by_race_in_year(subject, race, year)
+  #
+  # end
+
+
 end
--- test/statewide_test_repository_test.rb
+++ test/statewide_test_repository_test.rb
@@ -1,10 +1,16 @@
 require_relative 'test_helper'
+require 'csv'
 require_relative '../lib/statewide_test_repository'
+require_relative '../lib/data_parser'
 
 class StatewideTestRepositoryTest < Minitest::Test
   attr_reader :str
+
   def setup
     @str = StatewideTestRepository.new
+  end
+
+  def load_data
     str.load_data({
       :statewide_testing => {
         :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
@@ -13,13 +19,22 @@ class StatewideTestRepositoryTest < Minitest::Test
         :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
         :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
       }
-    })
+      })
   end
 
   def test_it_can_access_instance_of_statewide_test
+    str = StatewideTestRepository.new
+    str.load_data({
+      :statewide_testing => {
+        :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+        :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+        :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+      }
+      })
     statewide = str.find_by_name("ACADEMY 20")
-    assert_equal "ACADEMY 20", statewide
-    # assert_instance_of StatewideTest, statewide
+    assert_instance_of StatewideTest, statewide
   end
 
 end
